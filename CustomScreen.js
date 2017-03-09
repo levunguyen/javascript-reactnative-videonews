120ad3dcbcd0d8eb74fc3b689016cdf820982954
vanvo1605 <hongvant2@gmail.com>
2017-03-09 16:28:11 +0700

    Save data to local using AsyncStorage

--- CustomScreen.js
+++ CustomScreen.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import NavigationBar from 'react-native-navbar';
+import styles from  './style';
 import {
   Text,
-  View
+  View,
+  AsyncStorage,
+  Button
 } from 'react-native';
 
 
@@ -10,7 +13,25 @@ export default class CustomScreen extends Component {
   constructor(props) {
         super(props);
         console.log("CustomScreen contructor ", this.props.feedId);
+        this.state = {
+          favouriteList: []
+        }
     }
+
+  _saveDataLocal = async (feed) => {
+    console.log('Function add to my favourite list ...');
+    var myFavourite = await AsyncStorage.getItem("favouriteList");
+    if(myFavourite === null) {
+      await AsyncStorage.setItem("favouriteList", JSON.stringify(feed));
+      var afterAdded = await AsyncStorage.getItem("favouriteList");
+      console.log("Add first feed to my favourite: ", afterAdded);
+    } else {
+      await AsyncStorage.mergeItem("favouriteList", JSON.stringify(feed));
+      var afterAdded = await AsyncStorage.getItem("favouriteList");
+      console.log("Add one more feed to my favourite: ", afterAdded);
+    }
+  }
+
   render() {
     const leftButtonConfig = {
       title: 'Back',
@@ -22,7 +43,14 @@ export default class CustomScreen extends Component {
         <NavigationBar
           title={{ title: 'Custom screen', }}
           leftButton={leftButtonConfig} />
-          <Text>{this.props.name.title}</Text>
+        <Text>{this.props.name.title}</Text>
+        <View style={styles.initview.button}>
+          <Button
+            onPress={() => {this._saveDataLocal(this.props.name)}}
+            title="Add to Favourite"
+          />
+        </View>
+
       </View>
     );
   }
